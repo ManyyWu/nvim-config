c6f5e64e3b9a3acb4eeffd0fedf5a90fbd6f6aad
Your Name <you@example.com>
2022-06-03 18:48:26 +0000

    add gbk searching support

--- nvim/lua/plugins/nvim-spectre.lua
+++ nvim/lua/plugins/nvim-spectre.lua
@@ -2,4 +2,22 @@ local spectre = require("spectre")
 
 spectre.setup({
   open_cmd = 'new', -- 水平分割
+  mapping = {
+    ['toggle_gbk_encoding'] = {
+      map = "tg",
+      cmd = "<cmd>lua require('spectre').change_options('gbk')<CR>",
+      desc = "toggle gbk encoding"
+    },
+  },
+  find_engine = {
+    ['rg'] = {
+      options = {
+        ['gbk'] = {
+          value="--encoding=gbk",
+          desc="useing GBK",
+          icon="[G]"
+        },
+      }
+    },
+  }
 })

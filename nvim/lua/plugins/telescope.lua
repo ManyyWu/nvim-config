27f979467fe001c67cc0370528c4b4f7aa815d45
Your Name <you@example.com>
2023-04-01 14:51:40 +0000

    Revert "Update telescope.lua"
    This reverts commit ef762bea8f5b6a0d4d4052344f735f07027f680e.

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -23,49 +23,7 @@ telescope.setup({
       '--line-number',
       '--column',
       '--smart-case',
-      '--encoding=local telescope = require("telescope")
-
-local default = {
-  theme = "ivy", -- dropdown,cursor,ivy
-  layout_config = {
-    height = 0.4,
-  },
-  border = false,
-}
-
-telescope.setup({
-  defaults = {
-    layout_strategy = "horizontal",
-    layout_config = {
-      height = function(self, max_columns, max_lines) return max_lines end,
-      width = function(self, max_columns, max_lines) return max_columns end,
-    },
-    vimgrep_arguments = {
-      'rg',
-      '--color=never',
-      '--no-heading',
-      '--with-filename',
-      '--line-number',
-      '--column',
-      '--smart-case',
-      '--encoding=utf-8,gbk',
-    },
-  },
-  pickers = {
-    find_files  = default,
-    live_grep   = default,
-    grep_string = default,
-    buffers     = default,
-    help_tags   = default,
-    oldfiles    = default,
-    colorscheme = default,
-    diagnostics = default,
-  }
-})
-
--- Notes:
--- 在telescope窗口中C-q打开quicklist
-',
+      '--encoding=gbk',
     },
   },
   pickers = {
